b8c0448126672f9113574795fb5687b537ee3d38
github username <github email address>
2018-05-10 22:40:31 -0400

    workin

--- config/index.js
+++ config/index.js
@@ -1,5 +1,3 @@
 module.exports = {
   secret: process.env.NODE_ENV === "production" ? process.env.SECRET : "secret",
-  foursquareClientId: process.env.NODE_ENV === "production" ? process.env.foursquareClientId : "ZXKRNPVQX5DWUQZEHOH0SKN03KMVKMY232JSZZMMBSYZTYTO",
-  foursquareClientSecret: process.env.NODE_ENV === "production" ? process.env.foursquareClientSecret : "3JVIWVDVPUUL0P4JYK1K5UB0RQRUSA1O5RZ11VZMU4UAHMTG",
 };
--- routes/api/restaurants.js
+++ routes/api/restaurants.js
@@ -7,8 +7,9 @@ const request = require("request");
 const auth = require("../auth");
 const fetch = require("node-fetch");
 
-const CLIENT_ID = require("../../config").foursquareClientId;
-const CLIENT_SECRET = require("../../config").foursquareClientSecret;
+const CLIENT_ID = process.env.foursquareClientId;
+const CLIENT_SECRET = process.env.foursquareClientSecret;
+const GOOGLE_MAP_KEY = process.env.GOOGLE_MAP_KEY;
 
 router.post("/restaurants/search", auth.required, (req, res, next) => {
   Account.findById(req.payload.id).then((account) => {
@@ -19,69 +20,60 @@ router.post("/restaurants/search", auth.required, (req, res, next) => {
       longitude } = req.body;
     const searchGeo = `${latitude},${longitude}`;
     const baseUrl = "https://api.foursquare.com/v2/venues/explore?v=20170801&";
-    let params;
-    if (searchAddress) {
-      params = {
-        near: searchAddress,
-        query: searchTerm,
-        limit: 15,
-        client_id: CLIENT_ID,
-        client_secret: CLIENT_SECRET,
-      };
-    } else {
-      params = {
-        ll: searchGeo,
+    fetchLongLat(searchGeo, searchAddress).then((latLon) => {
+      const searchParams = {
+        ll: latLon,
         query: searchTerm,
         limit: 15,
         client_id: CLIENT_ID,
         client_secret: CLIENT_SECRET
       };
-    }
-    const esc = encodeURIComponent;
-    const query = Object.keys(params)
-      .map(k => esc(k) + "=" + esc(params[k]))
-      .join("&");
+      const esc = encodeURIComponent;
+      const query = Object.keys(searchParams)
+        .map(k => esc(k) + "=" + esc(params[k]))
+        .join("&");
       console.log("SEARCHING", query);
-    const url = baseUrl + query
-    fetch((url), {
-      method: "GET"
-    }).then(response => response.json())
-    .then((responseJson) => {
-      console.log('RESPONSE', responseJson);
-        if (!responseJson.response.groups || !responseJson.response.groups[0].items.length) {
-          const err = {
-            status: 400,
-            errorMessage: "No restaurants found",
+      const url = baseUrl + query
+      fetch((url), {
+        method: "GET"
+      }).then(response => response.json())
+        .then((responseJson) => {
+          console.log('RESPONSE', responseJson);
+          if (!responseJson.response.groups || !responseJson.response.groups[0].items.length) {
+            const err = {
+              status: 400,
+              errorMessage: "No restaurants found",
+            }
+            return next(err);
           }
-          return next(err);
-        }
-        const restaurants = Restaurant.parseSearch(responseJson.response.groups[0].items);
-        return res.json({ restaurants: restaurants });
-      });
-  }).catch(next);
-});
-
-router.post("/restaurants/add", auth.required, (req, res, next) => {
-  Account.findById(req.payload.id).then((account) => {
-    if (!account) { return next({ status: 401 }) }
-
-    const { groupId, restaurantData } = req.body;
-
-    const restaurant = new Restaurant(restaurantData);
-    Group.findById(groupId).then((group) => {
-      if (!group) { return next({ status: 401 }) }
-      restaurant.save().then(() => {
-        if (restaurant) {
-          console.log("Adding restaurant", restaurant);
-          group.addRestaurant(restaurant.id).then(() => {
-            return res.json({
-              item: restaurant
-            });
-          }).catch(next);
-        }
-      }).catch(next);
-    });
+          const restaurants = Restaurant.parseSearch(responseJson.response.groups[0].items);
+          return res.json({ restaurants: restaurants });
+        });
+    }).catch(next);
   });
+
+  router.post("/restaurants/add", auth.required, (req, res, next) => {
+    Account.findById(req.payload.id).then((account) => {
+      if (!account) { return next({ status: 401 }) }
+
+      const { groupId, restaurantData } = req.body;
+
+      const restaurant = new Restaurant(restaurantData);
+      Group.findById(groupId).then((group) => {
+        if (!group) { return next({ status: 401 }) }
+        restaurant.save().then(() => {
+          if (restaurant) {
+            console.log("Adding restaurant", restaurant);
+            group.addRestaurant(restaurant.id).then(() => {
+              return res.json({
+                item: restaurant
+              });
+            }).catch(next);
+          }
+        }).catch(next);
+      });
+    });
+    })
 });
 
 router.post("/restaurants/remove", auth.required, (req, res, next) => {
@@ -114,5 +106,22 @@ router.post("/restaurants/favorite", auth.required, (req, res, next) => {
     }).catch(next);
   });
 });
+
+function fetchLongLat(lonLat, searchAddress) {
+  return new Promise((resolve, reject) => {
+    if (lonLat) {
+      resolve(lonLat);
+    } else {
+      const searchTerm = searchAddress.split(" ").join("+")
+      const searchUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${searchTerm}&key=${GOOGLE_MAP_KEY}`;
+      fetch(url, {
+        method: "GET",
+      }).then((resp) => {
+        const latLon = `${resp.results[0].geometry.location.lat},${resp.results[0].geometry.location.lng}`;
+        resolve(latLon);
+      }).catch(reject());
+    }
+  })
+}
 
 module.exports = router;
